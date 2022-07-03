cc844a9335dee220b61567bd1715a28f3c31eb6e
tim <timrhymer@ya.ru>
2022-07-03 20:56:58 +0400

    refactoring complete

--- part1/practice/bad_smell_long_method/main.py
+++ part1/practice/bad_smell_long_method/main.py
@@ -11,53 +11,27 @@
 
 csv = """Вася;39\nПетя;26\nВасилий Петрович;9"""
 
-
 def get_users_list():
-    # Чтение данных из строки
-    data = []
-    for line in csv.split('\n'):
-        name, age = line.split(';')
-        data.append({'name': name, 'age': int(age)})
+    data = _read(csv)
+    sorted_data = _sort(data)
+    filtered_data = _filter(sorted_data)
+    return filtered_data
 
-    # Сортировка по возрасту по возрастанию
-    _new_data = []
-    used_person = set()
-    minimum_age_person = None
-    while len(used_person) != len(data):
-        if minimum_age_person is None:
-            for person in data:
-                if minimum_age_person is None:
-                    minimum_age_person = person
-                else:
-                    if person['name'] in used_person:
-                        continue
-                    else:
-                        if person['age'] < minimum_age_person['age']:
-                            minimum_age_person = person
-            _new_data.append(minimum_age_person)
-            used_person.add(minimum_age_person['name'])
-        local_minimum = None
-        for person in data:
-            if person['name'] in used_person:
-                continue
-            else:
-                if not local_minimum is None:
-                    if person['age'] < local_minimum['age']:
-                        local_minimum = person
-                else:
-                    local_minimum = person
-        _new_data.append(local_minimum)
-        used_person.add(local_minimum['name'])
 
-    # Фильтрация
-    result_data = []
-    for person in _new_data:
-        if person['age'] < 10:
-            continue
-        else:
-            result_data.append(person)
-    return result_data
+def _read(csv):
+    data = [x.split(";") for x in csv.split("\n")]
+    return data
+
+
+def _sort(data):
+    sorted_data = sorted(data, key=lambda x: int(x[1]))
+    return sorted_data
+
+def _filter(data):
+    filtered_data = [x for x in data if int(x[1]) > 10]
+    return filtered_data
 
 
 if __name__ == '__main__':
     print(get_users_list())
+
